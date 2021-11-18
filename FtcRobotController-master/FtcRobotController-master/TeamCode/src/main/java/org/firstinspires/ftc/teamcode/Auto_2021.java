109eeb92ec715104648bbbeee7f63cd8d949197e
Flame875 <55559362+Flame875@users.noreply.github.com>
2021-11-18 14:01:53 -0500

    Untested new auto, need to test with errors

---
+++ FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_2021.java
@@ -0,0 +1,183 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Auto Encoders", group="Linear Opmode")
+public class Auto_Encoders_2021 extends BaseAutoOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor drive_FL = null;
+    private DcMotor drive_RL = null;
+    private DcMotor drive_FR = null;
+    private DcMotor drive_RR = null;
+    private DcMotor leftArm = null;
+    private DcMotor rightArm = null;
+    private DcMotor armExtend = null;
+    private DcMotor feeder = null;
+    private Servo kickout = null;
+    private CRServo leftDucky = null;
+    private CRServo rightDucky = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        drive_FL = hardwareMap.get(DcMotor.class, "drive_FL");
+        drive_RL = hardwareMap.get(DcMotor.class, "drive_RL");
+        drive_FR = hardwareMap.get(DcMotor.class, "drive_FR");
+        drive_RR = hardwareMap.get(DcMotor.class, "drive_RR");
+        leftArm = hardwareMap.get(DcMotor.class, "left_Arm");
+        rightArm = hardwareMap.get(DcMotor.class, "right_Arm");
+        armExtend = hardwareMap.get(DcMotor.class, "extend_Arm");
+        feeder = hardwareMap.get(DcMotor.class, "feeder");
+
+        kickout = hardwareMap.get(Servo.class, "kickout");
+        leftDucky = hardwareMap.get(CRServo.class, "left_Ducky");
+        rightDucky = hardwareMap.get(CRServo.class, "right_Ducky");
+
+        leftArm.setDirection(DcMotor.Direction.FORWARD);
+        rightArm.setDirection(DcMotor.Direction.REVERSE);
+
+        leftArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        drive_FL.setDirection(DcMotor.Direction.FORWARD);
+        drive_RL.setDirection(DcMotor.Direction.FORWARD);
+        drive_FR.setDirection(DcMotor.Direction.REVERSE);
+        drive_RR.setDirection(DcMotor.Direction.REVERSE);
+        drive_FL.setDirection(DcMotor.Direction.FORWARD);
+        drive_RL.setDirection(DcMotor.Direction.FORWARD);
+        drive_FR.setDirection(DcMotor.Direction.REVERSE);
+        drive_RR.setDirection(DcMotor.Direction.FORWARD);
+
+        //Drive Modes
+        drive_FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drive_FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drive_RL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drive_RR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        waitForStart();
+        runtime.reset();
+
+        while (rightArm.getCurrentPosition() < degreesBore(40)) {
+
+            rightArm.setPower(-.5);
+            leftArm.setPower(-.5);
+
+        }
+
+        kickout.setPosition(1);
+        rightArm.setPower(0);
+        leftArm.setPower(0);
+
+        sleep(2000);
+
+        if (armExtend.getCurrentPosition() < 450) {
+
+            armExtend.setPower(.4);
+
+        }
+
+        armExtend.setPower(0);
+
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("FL Encoder Value",drive_FL.getCurrentPosition());
+        telemetry.addData("FR Encoder Value", drive_FR.getCurrentPosition());
+        telemetry.addData("RL Encoder Value", drive_RL.getCurrentPosition());
+        telemetry.addData("RR Encoder Value", drive_RR.getCurrentPosition());
+        telemetry.update();
+
+
+    }
+
+    public double inches(double input) {
+
+        final double     COUNTS_PER_MOTOR_REV    = 383.6;    // eg: GOBUILDA Motor Encoder
+        final double     DRIVE_GEAR_REDUCTION    = 1;     // This is < 1.0 if geared UP
+        final double     WHEEL_DIAMETER_INCHES   = 3.93701;     // For figuring circumference
+        final double     COUNTS_PER_INCH  = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * Math.PI);
+
+        return COUNTS_PER_INCH * input;
+
+    }
+
+    public double ticks(double input) {
+
+        final double COUNTS_PER_BORE_MOTOR_REV = 8192;    // eg: GOBUILDA Motor Encoder
+        final double COUNTS_TICKS_PER_REV_PER_DEGREE = (COUNTS_PER_BORE_MOTOR_REV) / 360;
+
+        return COUNTS_TICKS_PER_REV_PER_DEGREE * input;
+
+    }
+
+    public void runForwardsEncoder(double speed, double inputInches) {
+
+        double encoderValue = inches(inputInches);
+
+        while (drive_FL.getCurrentPosition() < encoderValue) {
+
+            drive_FL.setPower(speed);
+            drive_RL.setPower(speed);
+            drive_FR.setPower(speed);
+            drive_RR.setPower(speed);
+
+        }
+
+        drive_FL.setPower(0);
+        drive_RL.setPower(0);
+        drive_FR.setPower(0);
+        drive_RR.setPower(0);
+
+        ResetDriveEncoder();
+
+    }
+
+    public void ResetDriveEncoder(){
+
+        drive_FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        drive_RL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        drive_FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        drive_RR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+    }
+
+    public void turnOnEncoders() {
+
+        drive_FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        drive_RL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        drive_FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        drive_RR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public int degreesBore(int input) {
+
+        final int COUNTS_PER_BORE_MOTOR_REV = 8192;    // eg: GOBUILDA Motor Encoder
+        int COUNTS_TICKS_PER_REV_PER_DEGREE = (COUNTS_PER_BORE_MOTOR_REV) / 360;
+
+        return COUNTS_TICKS_PER_REV_PER_DEGREE * input;
+
+    }
+
+}}
